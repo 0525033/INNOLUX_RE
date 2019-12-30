2e5d00424fe81250fd73f3a64064ff7d7eaa8991
YuChen <46869238+0525033@users.noreply.github.com>
2019-12-30 12:26:35 +0800

    12301226

---
+++ js.js
@@ -0,0 +1,15 @@
+var x = 0;
+var i = 0;
+var content = document.getElementById("title");
+var NUM = ["<h1>Your Most Reliable Display Partner</h1><h3>We provide transparent demand and supply, real-time manufacturing information, and on-time delivery to serve our valuable customers.</h3>","<h1>Broaden Your Horizons</h1><h3>INNOLUX,the most valuable and competitive all-round solution provider for display technology.</h3>","<h1>Corporate Social Responsibility</h1><h3>INNOLUX, responsibly produce eco-friendly green products for a more sustainable enviroment.</h3>"];
+
+// 簡單內容輪播
+function change_content(){
+    if(i==3){i=0};
+    content.innerHTML=NUM[i];
+    i+=1;
+}
+setInterval(change_content,3000);
+// 簡單內容輪播
+
+document.getElementsByClassName("partner").onmouseover=function(){this.style.color="#000"}
